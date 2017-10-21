bb990878b78fbe17932620802f3831e796006ab0
O_Prime <sel-egor@yandex.ru>
2017-10-21 16:28:53 +0300

    implemented game logic

--- backend.js
+++ backend.js
@@ -1,5 +1,76 @@
-function sum(a, b) {
-  return a + b
+// tools
+const pipe = (x, f, ...fs) => f ? pipe(f(x), ...fs) : x
+
+const groupBy = (keyFn, xs) =>
+  xs.reduce((acc, x) => {
+    acc[keyFn(x)] = acc[keyFn(x)].concat(x)
+    return acc
+  }, {})
+
+const splitBy = (predicate, xs) =>
+  Object.values(groupBy(predicate, x))
+
+
+const makeGame = (status, moves, playedMoves) => ({
+  status: status,
+  moves: moves,
+  playedMoves: playedMoves
+})
+
+
+const newGame = () => makeGame('move of player X', '012345678', '')
+
+
+const allLines = [
+  '012', '345', '678',
+  '036', '147', '258',
+  '048', '246'
+]
+
+
+const sortByPlayer = playedMoves =>
+  playedMoves
+  .split('')
+  .reduce((acc, m, i) => {
+    acc[i % 2] = acc[i % 2].concat(m)
+    return acc
+  }, ['', ''])
+  .map(l => l.split('').sort().join(''))
+
+
+const isGameOver = playedMoves =>
+  pipe(
+    sortByPlayer(playedMoves),
+    ([p1, p2]) =>
+    allLines.some(line =>
+      pipe(
+        line.split(''),
+        lineArr =>
+        lineArr.every(l => p1.includes(l)) ||
+        lineArr.every(l => p2.includes(l)))))
+
+
+const xo = n => n % 2 === 0 ? 'O' : 'X'
+const winXO = ({ playedMoves }) => `win of player ${xo(playedMoves.length - 1)}`
+const moveXO = ({ moves }) => `move of player ${xo(moves.length)}`
+
+
+const makeMove = (move, game) =>
+  game.moves.split('').some(possibleMove => String(move) === possibleMove) ?
+  pipe(
+    [game.playedMoves.concat(move), game.moves.replace(move, '')],
+    ([playedMoves, moves]) =>
+    playedMoves.length === 9 ?
+    makeGame('tie', '', playedMoves) :
+    isGameOver(playedMoves) ?
+    makeGame(winXO(game), '', playedMoves) :
+    makeGame(moveXO(game), moves, playedMoves)) :
+  game
+
+
+const backend = {
+  newGame: newGame,
+  makeMove: makeMove
 }
 
-module.exports = sum
+module.exports = backend
--- backend.test.js
+++ backend.test.js
@@ -1,4 +1,94 @@
 const backend = require('./backend')
 
-test('adds 1 + 2 to equal 3', () =>
-  expect(backend(1, 2)).toBe(3))
+// tools
+const swap = f => (a, b) => f(b, a)
+const pipe = (x, f, ...fs) => f ? pipe(f(x), ...fs) : x
+
+const newGame = backend.newGame()
+
+const makeMove = backend.makeMove
+
+/*
+TODO: game field to win conditions function for testing
+`
+xo-
+xo-
+x--
+`
+*/
+describe('game has correct status', () => {
+  describe('`X` player wining game has `X` in it`s status', () => {
+    const testWinX = testCorrectStatus.bind(null, 'status contains `X`', 'X')
+
+    testWinX([0, 3, 1, 4, 2])
+  })
+
+
+  describe('`O` player wining game has `O` in it`s status', () => {
+    const testWinO = testCorrectStatus.bind(null, 'status contains `O`', 'O')
+
+    testWinO([0, 3, 1, 4, 6, 5])
+  })
+
+
+  describe('winning games`s status contains `win`', () => {
+    const testWin = testCorrectStatus.bind(null, 'status contains `win`', 'win')
+
+    testWin([0, 3, 1, 4, 2])
+    testWin([0, 1, 3, 4, 6])
+    testWin([0, 1, 4, 2, 7, 5, 8])
+    testWin([0, 3, 1, 4, 6, 5])
+  })
+
+
+  describe('ongoing game`s status contains `move`', () => {
+    const testOngoing = testCorrectStatus.bind(null, 'status contains `move`', 'move')
+
+    testOngoing([0])
+    testOngoing([0, 1])
+    testOngoing([0, 1, 2])
+    testOngoing([0, 1, 2, 3])
+    testOngoing([0, 1, 2, 3, 4])
+  })
+
+  describe('tied game`s status contains `tie`', () => {
+    const testTie = testCorrectStatus.bind(null, 'status contains `tie`', 'tie')
+
+    testTie([0, 1, 2, 3, 5, 4, 6, 8, 7])
+    testTie([0, 1, 2, 4, 3, 5, 7, 6, 8])
+  })
+
+  function testCorrectStatus(statement, expectedStatus, movesSequence) {
+    it(`${statement}, moves:${movesSequence}`, () =>
+      pipe(
+        movesSequence.reduce(swap(makeMove), newGame),
+        game => expect(game.status).toContain(expectedStatus)))
+  }
+})
+
+
+describe('player can`t play illegal moves', () => {
+  testIllegalMove(9)
+  testIllegalMove(12)
+  testIllegalMove(0, [0])
+  testIllegalMove(0, [0, 1, 2])
+  testIllegalMove(42, [1, 2, 3, 4])
+  testIllegalMove(4, [0, 3, 1, 4, 6, 5])
+
+  function testIllegalMove(illegalMove, playedMoves) {
+    it(`illegal move '${illegalMove}' is ignored, moves: ${playedMoves || "new game"}`, () => {
+      const gameSoFar = playedMoves ? playedMoves.reduce(swap(makeMove), newGame) : newGame
+      const afterIllegalMove = makeMove(illegalMove, gameSoFar)
+
+      expect(afterIllegalMove.playedMoves.length).toBe(gameSoFar.playedMoves.length)
+    })
+  }
+})
+
+
+// players make moves in turns
+// each player has own type of mark
+// win condition: 3 marks in 1 row, horizontally, vertically or diagonally
+
+// ... after each move game recalculates it's state:
+// move of one of the players or win of one of players or tie
