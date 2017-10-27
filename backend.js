1b50274c1268de69d0d3c22530f23bd2281530df
O_Prime <sel-egor@yandex.ru>
2017-10-27 16:28:19 +0300

    housekeeping

--- backend.js
+++ backend.js
@@ -36,7 +36,7 @@ const isGameOver = moves => pipe(
     movesInLineCount(oMoves, l) === 3))
 
 
-const xo = n => n % 2 === 0 ? 'X' : 'O'
+const xo = n => isEven(n) ? 'X' : 'O'
 const winXO = moves => `win of player ${xo(moves.length)}`
 const moveXO = moves => `move of player ${xo(moves.length)}`
 
@@ -57,5 +57,6 @@ module.exports = {
   allLines: allLines,
   splitToXO: splitToXO,
   movesInLineCount: movesInLineCount,
-  canonize: canonize
+  canonize: canonize,
+  xo: xo
 }
--- testHelpers.js
+++ testHelpers.js
@@ -5,15 +5,14 @@ const xo = n => n % 2 === 0 ? 'X' : 'O'
 const addNewLines = (acc, x, i) => acc.concat(i % 3 == 0 ? `\n${x}` : x)
 
 
-const visualizeGameField = (moves) =>
-  pipe(
-    '---------'.split(''),
-    field => !moves ? field :
-    field.map((cell, cellIdx) => pipe(
-      moves.findIndex(move => move == cellIdx),
-      moveIdx => moveIdx != -1 ? xo(moveIdx) : cell)),
-    field => field.reduce(addNewLines, []),
-  ).join('')
+const visualizeGameField = moves => pipe(
+  '---------'.split(''),
+  field => !moves ? field :
+  field.map((cell, cellIdx) => pipe(
+    moves.findIndex(move => move == cellIdx),
+    moveIdx => moveIdx != -1 ? xo(moveIdx) : cell)),
+  field => field.reduce(addNewLines, []),
+).join('')
 
 
 module.exports = {
